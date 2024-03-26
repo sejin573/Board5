d07185aec13d495431b834c7c400e50847544395
HeoTaehoon7 <greenclass.hoon@gmail.com>
2024-03-26 16:00:33 +0900

    Board List

--- src/main/java/com/board/Board5Application.java
+++ src/main/java/com/board/Board5Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Board4Application {
+public class Board5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Board4Application.class, args);
+		SpringApplication.run(Board5Application.class, args);
 	}
 
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -20,6 +20,9 @@
 	<div>&nbsp;</div>
 	<div><a href="/Users/List">사용자 목록</a></div>
 	<div><a href="/Users/WriteForm">사용자 추가</a></div>
+	<div>&nbsp;</div>	
+	<div><a href="/Board/List?menu_id=MENU01">게시물 목록</a></div>
+	<div><a href="/Board/WriteForm?menu_id=MENU01">게시물 등록</a></div>
   </main>	
 </body>
 </html>
